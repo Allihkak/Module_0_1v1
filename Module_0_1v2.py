58745419054eb07096e6eb9528c5fdc742647dab
Allihkak <chertenok231@gmail.com>
2024-11-20 22:43:53 +0500

    Add files via upload

---
+++ Module_0_1v2.py
@@ -0,0 +1,11 @@
+#1st program
+print((9**0.5)*5)
+#2nd program
+print(9.9>9.8) and (1000!=1000.1)
+#3rd program
+print((2*2+2)==(2*(2+2)))
+#4th program
+a=float('123.456')
+b=10
+c=a*b
+print(int(c)%10)
